9d96041e237ddda018a51ebc103509a1c7755d1f
codebadolo <codewithbadolo@gmail.com>
2025-01-14 14:55:24 +0000

    db fixed

--- sig/settings.py
+++ sig/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'sig.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 # 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -109,13 +109,13 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
-DATABASES = {
+
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / "db.sqlite3",
     }
-}
+}'''
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
