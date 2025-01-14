42212ea4abc71caebba8842aa49743338871fabe
codebadolo <codewithbadolo@gmail.com>
2025-01-14 11:36:37 +0000

    db tooggle

--- sig/settings.py
+++ sig/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'sig.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sicsnasara_dev',
@@ -107,14 +107,14 @@ WSGI_APPLICATION = 'sig.wsgi.application'
         'PASSWORD': DEV_DATABASE_PASSWORD,
         'PORT': '5432',
     }
-}'''
-
+}
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / "db.sqlite3",
     }
-}
+}'''
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
