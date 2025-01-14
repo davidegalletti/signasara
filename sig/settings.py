275cc783ebe4d78685ec6cac398786d6be6d6d4d
codebadolo <codewithbadolo@gmail.com>
2025-01-14 00:31:19 +0000

    db switched

--- sig/settings.py
+++ sig/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'sig.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sicsnasara_dev',
@@ -108,13 +108,13 @@ WSGI_APPLICATION = 'sig.wsgi.application'
         'PORT': '5432',
     }
 }
-'''
-DATABASES = {
+
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / "db.sqlite3",
     }
-}
+}'''
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
