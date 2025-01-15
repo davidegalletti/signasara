c670cc61891ca85afb7668042855600b27ccf6b5
codebadolo <codewithbadolo@gmail.com>
2025-01-15 11:24:43 +0000

    costum amount tag

--- scuelo/templatetags/custom_filters.py
+++ scuelo/templatetags/custom_filters.py
@@ -15,3 +15,10 @@ def format_date_obj(value, date_format="%d/%m/%y"):
         return value.strftime(date_format)
     return ""
 
+@register.filter
+def format_amount(value):
+    try:
+        value = int(value)
+        return f"{value:,}".replace(",", " ") + " FCFA"
+    except (ValueError, TypeError):
+        return value
