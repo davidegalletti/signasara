ae5b4c27b7b87834e1b4a73421da04202ab8bde0
codebadolo <codewithbadolo@gmail.com>
2025-01-15 10:53:25 +0000

    costum filters

---
+++ scuelo/templatetags/custom_filters.py
@@ -0,0 +1,17 @@
+from django import template
+from datetime import datetime
+
+register = template.Library()
+
+@register.filter
+def format_date(value, date_format="%d/%m/%y"):
+    if isinstance(value, datetime):
+        return value.strftime(date_format)
+    return value
+
+@register.filter
+def format_date_obj(value, date_format="%d/%m/%y"):
+    if value:
+        return value.strftime(date_format)
+    return ""
+
